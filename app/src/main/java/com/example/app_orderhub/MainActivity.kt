6e06ffc70836b32b7a16e12ba9a2064c1ed73944
JuliaIFreitas <julia.freitas@sptech.school>
2025-02-24 19:31:31 -0300

    Enviando início do desenvolvimento da tela

--- app/src/main/java/com/example/app_orderhub/MainActivity.kt
+++ app/src/main/java/com/example/app_orderhub/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.app_orderhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -13,25 +20,34 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.app_orderhub.util.theme.AppOrderHubTheme
+import kotlin.io.path.Path
+import kotlin.io.path.moveTo
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +67,43 @@ fun esqueciASenhaPreview() {
 }
 
 @Composable
+fun WaveAnimation(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val waveOffset by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    Canvas(modifier = modifier
+        .fillMaxWidth()
+        .height(100.dp)) {
+        val waveAmplitude = 30f
+        val waveLength = size.width / 1f
+        val path = androidx.compose.ui.graphics.Path().apply {
+            moveTo(0f, size.height / 2f)
+
+            for (i in 0..size.width.toInt()) {
+                val x = i.toFloat()
+                val y = (waveAmplitude * sin((2 * Math.PI / waveLength) * (x + waveOffset * waveLength)) + size.height / 10).toFloat()
+                lineTo(x, y)
+            }
+
+            lineTo(size.width, size.height)
+            lineTo(1f, size.height)
+            close()
+        }
+
+        clipPath(path) {
+            drawRect(color = Color.Blue)
+        }
+    }
+}
+
+@Composable
 fun EsqueciASenha() {
     var nome = remember {
         mutableStateOf("")
@@ -64,6 +117,8 @@ fun EsqueciASenha() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        WaveAnimation(modifier = Modifier.height(250.dp))
+
         Text(
 //            modifier = Modifier
 //                .border(2.dp, Color.Red),
@@ -100,7 +155,8 @@ fun EsqueciASenha() {
             colors = OutlinedTextFieldDefaults.colors( unfocusedContainerColor = Color.White ),
             
             modifier = Modifier
-                .height(50.dp)
+                .fillMaxWidth(.73f)
+//                .height(50.dp)
                 .background(Color.White)
                 .border(2.dp, Color.Gray, RoundedCornerShape(12.dp))
                 .clip(RoundedCornerShape(12.dp)),
@@ -110,7 +166,15 @@ fun EsqueciASenha() {
                     text = "username@orderhub.com"
                 )
             },
-            onValueChange = {
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Email,
+                    contentDescription = "Ícone de e-mail",
+                    tint = Color.Gray
+                )
+            },
+
+                    onValueChange = {
                     novoValor ->
                 nome.value = novoValor
             }
