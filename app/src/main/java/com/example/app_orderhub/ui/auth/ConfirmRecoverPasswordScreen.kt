c1dc59a5e102a91dcdb669eb1e32744b5622d453
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-02-27 19:06:15 -0300

    ajuste no button

---
+++ app/src/main/java/com/example/app_orderhub/ui/auth/ConfirmRecoverPasswordScreen.kt
@@ -0,0 +1,11 @@
+package com.example.app_orderhub.ui.auth
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ConfirmRecoverPasswordScreen(navController: NavController) {
+
+}
+
+
--- app/src/main/java/com/example/app_orderhub/ui/auth/RecoverPasswordScreen.kt
+++ app/src/main/java/com/example/app_orderhub/ui/auth/RecoverPasswordScreen.kt
@@ -1,11 +1,9 @@
 package com.example.app_orderhub.ui.auth
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -23,14 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.room.util.copy
 import com.example.app_orderhub.data.Client
 import com.example.app_orderhub.ui.auth.components.Input
 import com.example.app_orderhub.ui.auth.components.TitleSubtitle
