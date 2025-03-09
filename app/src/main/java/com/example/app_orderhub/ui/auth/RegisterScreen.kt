e95fd535ea6f381694827ae8d6afc3133e38fc92
Kaiquevolpe <kaique.silva@sptech.school>
2025-03-09 16:31:15 -0300

    Atualização da tela de cadastro

--- app/src/main/java/com/example/app_orderhub/ui/auth/RegisterScreen.kt
+++ app/src/main/java/com/example/app_orderhub/ui/auth/RegisterScreen.kt
@@ -13,9 +13,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -30,6 +35,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -38,6 +44,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.app_orderhub.data.Client
+import com.example.app_orderhub.ui.auth.components.ButtonAuth
+import com.example.app_orderhub.ui.auth.components.DividerSpace
+import com.example.app_orderhub.ui.auth.components.ImageTop
+import com.example.app_orderhub.ui.auth.components.Input
+import com.example.app_orderhub.ui.auth.components.Title
+import com.example.app_orderhub.util.theme.ColorBackGroundDefault
+import com.example.app_orderhub.util.theme.OrderHubBlue
 
 @Composable
 fun RegisterScreen(navController: NavController) {
@@ -56,78 +70,100 @@ fun RegisterScreen(navController: NavController) {
 
 @Composable
 fun RegisterScreenPreviw() {
-    var name by remember { mutableStateOf(TextFieldValue("")) }
-    var lastName by remember { mutableStateOf(TextFieldValue("")) }
-    var email by remember { mutableStateOf(TextFieldValue("")) }
-    var password by remember { mutableStateOf(TextFieldValue("")) }
+
+    var client = remember { mutableStateOf(Client()) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
-            .padding(horizontal = 20.dp)
-            .padding(top = 250.dp), // Pequena redução no padding superior
+            .background(ColorBackGroundDefault),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Faça seu cadastro",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
+        ImageTop()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Title(title = "Faça seu cadastro")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Input(
+            value = client.value.nome,
+            onValueChange = { newValue -> client.value = client.value.copy(nome = newValue) },
+            label = "Nome",
+            widthPercentage = 0.8f,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "AccountCircle Icon"
+                )
+            },
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Input(
+            value = client.value.sobrenome,
+            onValueChange = { newValue -> client.value = client.value.copy(sobrenome = newValue) },
+            label = "Sobrenome",
+            widthPercentage = 0.8f,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "AccountCircle Icon"
+                )
+            },
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Input(
+            value = client.value.email,
+            onValueChange = { newValue -> client.value = client.value.copy(email = newValue) },
+            label = "E-mail",
+            widthPercentage = 0.8f,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Lock Icon"
+                )
+            },
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Input(
+            value = client.value.senha,
+            onValueChange = { newValue -> client.value = client.value.copy(senha = newValue) },
+            label = "Senha",
+            widthPercentage = 0.8f,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "Lock Icon"
+                )
+            },
+            keyboardType = KeyboardType.Password,
+            visualTransformation = PasswordVisualTransformation()
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Nome*") },
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = lastName,
-            onValueChange = { lastName = it },
-            label = { Text("Sobrenome*") },
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("E-mail*") },
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Senha*") },
-            visualTransformation = PasswordVisualTransformation(),
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {  },
-            modifier = Modifier
-                .width(270.dp)
-                .align(Alignment.CenterHorizontally),
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White,
-                contentColor = Color.Black
-            ),
-            border = BorderStroke(1.dp, Color.Black)
-        ) {
-            Text("Continuar")
+        Box() {
+            ButtonAuth(
+                borderRadius = 10,
+                text = "Continuar",
+                textColor = Color.Black,
+                backgroundColor = Color.White,
+                fontSize = 20,
+                borderWidth = 1,
+            )
         }
 
 
@@ -150,35 +186,15 @@ fun RegisterScreenPreviw() {
         Spacer(modifier = Modifier.height(10.dp))
 
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Divider(
-                color = Color.Gray,
-                modifier = Modifier
-                    .weight(1f)
-                    .height(2.dp)
-            )
-            Text(
-                text = " Conectar usando ",
-                color = Color.Gray,
-                fontSize = 13.sp,
-                modifier = Modifier.padding(horizontal = 6.dp),
-                textAlign = TextAlign.Center
-            )
-            Divider(
-                color = Color.Gray,
-                modifier = Modifier
-                    .weight(1f)
-                    .height(2.dp)
-            )
-        }
+       DividerSpace(content = "Continuar Usando")
+
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewSignUpScreen() {
     RegisterScreenPreviw()
 }
+
