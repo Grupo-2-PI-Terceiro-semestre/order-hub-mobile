22732843bbc38b64e74216df78a07ac38c0650ae
JuliaIFreitas <julia.freitas@sptech.school>
2025-03-12 20:48:18 -0300

    Ajuste na organização de pastas da agenda

--- app/src/main/java/com/example/app_orderhub/ui/catolog/components/CreateSchedule.kt
+++ app/src/main/java/com/example/app_orderhub/ui/catolog/components/CreateSchedule.kt
@@ -1,4 +1,4 @@
-package com.example.app_orderhub.ui.schedule.components
+package com.example.app_orderhub.ui.catolog.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
