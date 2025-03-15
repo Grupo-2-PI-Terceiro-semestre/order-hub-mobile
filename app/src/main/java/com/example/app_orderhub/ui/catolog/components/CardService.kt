3c5815c66d7216933736d25eac917fe0daa2a25f
JuliaIFreitas <julia.freitas@sptech.school>
2025-03-15 18:57:45 -0300

    Início do desenvolvimento do modal de agenda

---
+++ app/src/main/java/com/example/app_orderhub/ui/catolog/components/CardService.kt
@@ -0,0 +1,17 @@
+package com.example.app_orderhub.ui.catolog.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Composable
+fun CardService(navController: NavController) {
+    CardServicePreview()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardServicePreview() {
+    
+}
