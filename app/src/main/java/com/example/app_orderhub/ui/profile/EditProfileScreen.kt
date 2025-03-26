06cf8190ef701854edbb743e1ea6d4a0c56c93e4
guilhermecruci <guilherme.cruci@sptech.school>
2025-03-25 23:04:59 -0300

    Subindo parte da Tela EditProfileScreen

--- app/src/main/java/com/example/app_orderhub/ui/profile/EditProfileScreen.kt
+++ app/src/main/java/com/example/app_orderhub/ui/profile/EditProfileScreen.kt
@@ -1,32 +1,29 @@
 package com.example.app_orderhub.ui.profile
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.app_orderhub.navigation.MenuNavigation
-import com.example.app_orderhub.util.theme.ColorBackGroundDefault
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.app_orderhub.ui.profile.components.OptionProfile
+import com.example.app_orderhub.ui.profile.components.PicProfile
 
 @Composable
-fun EditProfileScreen(navController: NavController) {
-    MenuNavigation(navController) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(ColorBackGroundDefault),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "EditProfileScreen Screen",
-                color = Color.Black,
-                fontSize = 24.sp
-            )
-        }
+fun EditProfileScreen(modifier: Modifier = Modifier) {
+Column {
+
+    OptionProfile()
+    Spacer(modifier = Modifier.height(20.dp))
+    PicProfile()
+    Spacer(modifier = Modifier.height(20.dp))
+
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun EditProfileScreenPrev() {
+    EditProfileScreen()
 }
---
+++ app/src/main/java/com/example/app_orderhub/ui/profile/components/InputName.kt
@@ -0,0 +1,79 @@
+package com.example.app_orderhub.ui.profile.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun InputName(modifier: Modifier = Modifier) {
+    var name by remember {
+        mutableStateOf("")
+    }
+    var lastName by remember {
+        mutableStateOf("")
+    }
+Column {
+
+    CustomTextFild(modifier = Modifier
+        .padding(12.dp)
+        .fillMaxWidth(),
+        valueOf = name,
+        labelText = "Qual o seu Primeiro Nome?",
+        changeValue ={novoValor ->
+            name = novoValor
+        }
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+
+    CustomTextFild(modifier = Modifier
+        .padding(12.dp)
+
+        .fillMaxWidth(),
+        valueOf = lastName,
+        labelText = "E seu Sobrenome?",
+        changeValue ={novoValor ->
+            lastName = novoValor
+        }
+    )
+}
+    }
+
+
+@Preview
+@Composable
+private fun InputNamePrev() {
+    InputName()
+}
+
+
+@Composable
+fun CustomTextFild(
+    modifier: Modifier = Modifier,
+    valueOf : String,
+    changeValue: (String) -> Unit,
+    labelText: String,
+) {
+    TextField(
+        value = valueOf,
+        label = {
+            Text(text = labelText)
+        },
+        modifier = modifier,
+        onValueChange = {
+            changeValue(it)
+        }
+    )
+}
