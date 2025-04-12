9432bd0fefabb1feb8746e6ff5a0718f0654e3cc
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-04-12 16:17:48 -0300

    Subindo Module

--- app/src/main/java/com/example/app_orderhub/di/AppModule.kt
+++ app/src/main/java/com/example/app_orderhub/di/AppModule.kt
@@ -2,10 +2,12 @@ package com.example.app_orderhub.di
 
 import com.example.app_orderhub.data.remote.AuthApi
 import com.example.app_orderhub.data.remote.CatalogApi
+import com.example.app_orderhub.data.remote.ClientApi
 import com.example.app_orderhub.data.remote.HomeApi
 import com.example.app_orderhub.data.remote.config.RetrofitClient
 import com.example.app_orderhub.data.repository.AuthRepository
 import com.example.app_orderhub.data.repository.CatalogRepository
+import com.example.app_orderhub.data.repository.ClientRepository
 import com.example.app_orderhub.data.repository.HomeRepository
 
 object AppModule {
@@ -29,4 +31,10 @@ object AppModule {
 
     // Cria a instância do Repository
     val catalogRepository: CatalogRepository = CatalogRepository(catalogApi)
+
+
+    // Cria a instância da API
+    private val clientApi: ClientApi = RetrofitClient.createApi(ClientApi::class.java)
+
+    val clientRepository: ClientRepository = ClientRepository(clientApi)
 }
