574a6c705988351e7ca8660f347a78787cda7e87
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-06-15 19:59:00 -0300

    Ajustando Dev

--- app/src/main/java/com/example/app_orderhub/data/model/catalog/CatologRequest.kt
+++ app/src/main/java/com/example/app_orderhub/data/model/catalog/CatologRequest.kt
@@ -1,9 +1,10 @@
 package com.example.app_orderhub.data.model.catalog
 
 data class ScheduleRequest(
+    val idAgendamento: String,
     val idCliente: Int,
     val idServico: Int,
     val idProfissional: Int,
     val dataAgendamento: String,
-    val statusAgendamento: String
+    val statusAgendamento: String? = null
 )
