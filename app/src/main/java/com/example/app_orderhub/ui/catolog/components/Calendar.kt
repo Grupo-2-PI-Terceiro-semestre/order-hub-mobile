493eb11093b3425bf51032431bdf5777964447ee
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2025-06-15 19:56:21 -0300

    Ajustando Dev

--- app/src/main/java/com/example/app_orderhub/ui/catolog/components/Calendar.kt
+++ app/src/main/java/com/example/app_orderhub/ui/catolog/components/Calendar.kt
@@ -7,9 +7,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
 
 @Composable
-fun Calendar(onDateSelected: (String) -> Unit) {
+fun Calendar(
+    initialDate: String? = null,
+    onDateSelected: (String) -> Unit) {
     AndroidView(
         modifier = Modifier.fillMaxWidth(),
         factory = { ctx ->
@@ -18,6 +23,16 @@ fun Calendar(onDateSelected: (String) -> Unit) {
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT
                 )
+
+                initialDate?.let { dateStr ->
+                    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                    val localDate = LocalDate.parse(dateStr, formatter)
+                    val calendar = Calendar.getInstance().apply {
+                        set(localDate.year, localDate.monthValue - 1, localDate.dayOfMonth)
+                    }
+                    date = calendar.timeInMillis
+                }
+
                 setOnDateChangeListener { _, year, month, dayOfMonth ->
                     val selectedDate = "$dayOfMonth/${month + 1}/$year"
                     onDateSelected(selectedDate)
